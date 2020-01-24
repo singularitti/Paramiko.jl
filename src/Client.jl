fedaafabb8573b0e7927ae93028a9ab559216a0b
Qi Zhang <singularitti@outlook.com>
2020-01-24 02:26:29 -0500

    :bug: Fix `KeyError: key "o" not found`

--- src/Client.jl
+++ src/Client.jl
@@ -29,21 +29,23 @@ PyCall.pycall(f::SSHClient, args...; kws...) = pycall(PyObject(f), args...; kws.
 Base.Docs.doc(f::SSHClient) = Base.Docs.doc(PyObject(f))
 # Code from https://github.com/JuliaPy/PyPlot.jl/blob/6b38c75/src/PyPlot.jl#L65-L71
 Base.getproperty(f::SSHClient, s::Symbol) = getproperty(PyObject(f), s)
+Base.getproperty(f::SSHClient, s::AbstractString) = getproperty(PyObject(f), s)
 Base.setproperty!(f::SSHClient, s::Symbol, x) = setproperty!(PyObject(f), s, x)
+Base.setproperty!(f::SSHClient, s::AbstractString, x) = setproperty!(PyObject(f), s, x)
 PyCall.hasproperty(f::SSHClient, s::Symbol) = hasproperty(PyObject(f), s)
 Base.propertynames(f::SSHClient) = propertynames(PyObject(f))
 Base.haskey(f::SSHClient, x) = haskey(PyObject(f), x)
 
-Base.close(f::SSHClient) = f.o.close()
-Sockets.connect(f::SSHClient, hostname, args...; kws...) = f.o.connect(hostname, args...; kws...)
+Base.close(f::SSHClient) = PyObject(f).close()
+Sockets.connect(f::SSHClient, hostname, args...; kws...) = PyObject(f).connect(hostname, args...; kws...)
 
-exec_command(f::SSHClient, command::AbstractString, args...; kws...) = f.o.exec_command(command, args..., kws...)
+exec_command(f::SSHClient, command::AbstractString, args...; kws...) = PyObject(f).exec_command(command, args..., kws...)
 exec_command(f::SSHClient, command::Base.AbstractCmd, args...; kws...) = exec_command(f, string(command), args...; kws...)
-gethostkeys(f::SSHClient) = f.o.get_host_keys()
-gettransport(f::SSHClient) = f.o.get_transport()
-invokeshell(f::SSHClient, args...; kws...) = f.o.invoke_shell(args...; kws...)
-loadhostkeys(f::SSHClient, filename::AbstractString) = f.o.load_host_keys(filename)
-load_system_host_keys(f::SSHClient, filename::AbstractString) = f.o.load_system_host_keys(filename)
-opensftp(f::SSHClient) = f.o.open_sftp()
+gethostkeys(f::SSHClient) = PyObject(f).get_host_keys()
+gettransport(f::SSHClient) = PyObject(f).get_transport()
+invokeshell(f::SSHClient, args...; kws...) = PyObject(f).invoke_shell(args...; kws...)
+loadhostkeys(f::SSHClient, filename::AbstractString) = PyObject(f).load_host_keys(filename)
+load_system_host_keys(f::SSHClient, filename::AbstractString) = PyObject(f).load_system_host_keys(filename)
+opensftp(f::SSHClient) = PyObject(f).open_sftp()
 
 end # module Client
