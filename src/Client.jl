543483ec64da7c43d6f240cafce037bc99d0e7bd
Qi Zhang <singularitti@outlook.com>
2020-01-24 02:18:41 -0500

    :art: Write method extensions explicitly

--- src/Client.jl
+++ src/Client.jl
@@ -3,9 +3,9 @@ module Client
 using PyCall: PyObject, PyAny, pycall
 
 import ..paramiko
-
+# Extending methods
 import Sockets
-import PyCall: PyObject, pycall
+import PyCall
 
 export SSHClient
 export exec_command,
@@ -18,25 +18,25 @@ mutable struct SSHClient
 end
 SSHClient() = SSHClient(paramiko.SSHClient())
 
-PyObject(f::SSHClient) = getfield(f, :o)
-convert(::Type{SSHClient}, o::PyObject) = SSHClient(o)
-==(f::SSHClient, g::SSHClient) = PyObject(f) == PyObject(g)
-==(f::SSHClient, g::PyObject) = PyObject(f) == g
-==(f::PyObject, g::SSHClient) = f == PyObject(g)
-hash(f::SSHClient) = hash(PyObject(f))
-pycall(f::SSHClient, args...; kws...) = pycall(PyObject(f), args...; kws...)
+PyCall.PyObject(f::SSHClient) = getfield(f, :o)
+Base.convert(::Type{SSHClient}, o::PyObject) = SSHClient(o)
+Base.:(==)(f::SSHClient, g::SSHClient) = PyObject(f) == PyObject(g)
+Base.:(==)(f::SSHClient, g::PyObject) = PyObject(f) == g
+Base.:(==)(f::PyObject, g::SSHClient) = f == PyObject(g)
+Base.hash(f::SSHClient) = hash(PyObject(f))
+PyCall.pycall(f::SSHClient, args...; kws...) = pycall(PyObject(f), args...; kws...)
 (f::SSHClient)(args...; kws...) = pycall(PyObject(f), PyAny, args...; kws...)
 Base.Docs.doc(f::SSHClient) = Base.Docs.doc(PyObject(f))
-# Code from https://github.com/JuliaPy/PyPlot.jl/blob/6b38c75/src/PyPlot.jl#L64-L71
-# Note: using `Union{Symbol,String}` produces ambiguity.
+# Code from https://github.com/JuliaPy/PyPlot.jl/blob/6b38c75/src/PyPlot.jl#L65-L71
 Base.getproperty(f::SSHClient, s::Symbol) = getproperty(PyObject(f), s)
 Base.setproperty!(f::SSHClient, s::Symbol, x) = setproperty!(PyObject(f), s, x)
-hasproperty(f::SSHClient, s::Symbol) = hasproperty(PyObject(f), s)
+PyCall.hasproperty(f::SSHClient, s::Symbol) = hasproperty(PyObject(f), s)
 Base.propertynames(f::SSHClient) = propertynames(PyObject(f))
-haskey(f::SSHClient, x) = haskey(PyObject(f), x)
+Base.haskey(f::SSHClient, x) = haskey(PyObject(f), x)
 
 Base.close(f::SSHClient) = f.o.close()
 Sockets.connect(f::SSHClient, hostname, args...; kws...) = f.o.connect(hostname, args...; kws...)
+
 exec_command(f::SSHClient, command::AbstractString, args...; kws...) = f.o.exec_command(command, args..., kws...)
 exec_command(f::SSHClient, command::Base.AbstractCmd, args...; kws...) = exec_command(f, string(command), args...; kws...)
 gethostkeys(f::SSHClient) = f.o.get_host_keys()
