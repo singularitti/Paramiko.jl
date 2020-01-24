7460500bfcb3b86c48b4742b2519944f3058f0e7
Qi Zhang <singularitti@outlook.com>
2020-01-24 02:43:58 -0500

    Add `global version`

--- src/init.jl
+++ src/init.jl
@@ -8,4 +8,11 @@ const paramiko = PyNULL()
 
 function __init__()
     copy!(paramiko, pyimport("paramiko"))
+    # Code from https://github.com/JuliaPy/PyPlot.jl/blob/caf7f89/src/init.jl#L168-L173
+    vers = paramiko.__version__
+    global version = try
+        vparse(vers)
+    catch
+        v"0.0.0" # fallback
+    end
 end
