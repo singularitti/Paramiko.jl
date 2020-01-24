7a96213d06de48add4166ddf74f6c9e877d122e0
Qi Zhang <singularitti@outlook.com>
2020-01-24 17:57:34 -0500

    Implement `SFTPClient` type

--- src/Client.jl
+++ src/Client.jl
@@ -7,34 +7,15 @@ import ..paramiko
 import Sockets
 import PyCall
 
-export SSHClient
+export SSHClient, SFTPClient
 export exec_command,
     gethostkeys, gettransport, invokeshell, loadhostkeys, load_system_host_keys, opensftp
 
-# Code from https://github.com/JuliaPy/PyPlot.jl/blob/6b38c75/src/PyPlot.jl#L49-L62
 # Wrapper around `paramiko.client.SSHClient`
 mutable struct SSHClient
     o::PyObject
 end
 SSHClient() = SSHClient(paramiko.SSHClient())
-
-PyCall.PyObject(f::SSHClient) = getfield(f, :o)
-Base.convert(::Type{SSHClient}, o::PyObject) = SSHClient(o)
-Base.:(==)(f::SSHClient, g::SSHClient) = PyObject(f) == PyObject(g)
-Base.:(==)(f::SSHClient, g::PyObject) = PyObject(f) == g
-Base.:(==)(f::PyObject, g::SSHClient) = f == PyObject(g)
-Base.hash(f::SSHClient) = hash(PyObject(f))
-PyCall.pycall(f::SSHClient, args...; kws...) = pycall(PyObject(f), args...; kws...)
-(f::SSHClient)(args...; kws...) = pycall(PyObject(f), PyAny, args...; kws...)
-Base.Docs.doc(f::SSHClient) = Base.Docs.doc(PyObject(f))
-# Code from https://github.com/JuliaPy/PyPlot.jl/blob/6b38c75/src/PyPlot.jl#L65-L71
-Base.getproperty(f::SSHClient, s::Symbol) = getproperty(PyObject(f), s)
-Base.getproperty(f::SSHClient, s::AbstractString) = getproperty(PyObject(f), s)
-Base.setproperty!(f::SSHClient, s::Symbol, x) = setproperty!(PyObject(f), s, x)
-Base.setproperty!(f::SSHClient, s::AbstractString, x) = setproperty!(PyObject(f), s, x)
-PyCall.hasproperty(f::SSHClient, s::Symbol) = hasproperty(PyObject(f), s)
-Base.propertynames(f::SSHClient) = propertynames(PyObject(f))
-Base.haskey(f::SSHClient, x) = haskey(PyObject(f), x)
 
 Base.close(f::SSHClient) = PyObject(f).close()
 Sockets.connect(f::SSHClient, hostname, args...; kws...) = PyObject(f).connect(hostname, args...; kws...)
@@ -48,4 +29,32 @@ loadhostkeys(f::SSHClient, filename::AbstractString) = PyObject(f).load_host_key
 load_system_host_keys(f::SSHClient, filename = nothing) = PyObject(f).load_system_host_keys(filename)
 opensftp(f::SSHClient) = PyObject(f).open_sftp()
 
-end # module Client
+mutable struct SFTPClient
+    o::PyObject
+end
+SFTPClient() = SFTPClient(paramiko.SFTPClient())
+
+for T in (:SSHClient, :SFTPClient)
+    eval(quote
+        # Code from https://github.com/JuliaPy/PyPlot.jl/blob/6b38c75/src/PyPlot.jl#L54-L62
+        PyCall.PyObject(f::$T) = getfield(f, :o)
+        Base.convert(::Type{$T}, o::PyObject) = $T(o)
+        Base.:(==)(f::$T, g::$T) = PyObject(f) == PyObject(g)
+        Base.:(==)(f::$T, g::PyObject) = PyObject(f) == g
+        Base.:(==)(f::PyObject, g::$T) = f == PyObject(g)
+        Base.hash(f::$T) = hash(PyObject(f))
+        PyCall.pycall(f::$T, args...; kws...) = pycall(PyObject(f), args...; kws...)
+        (f::$T)(args...; kws...) = pycall(PyObject(f), PyAny, args...; kws...)
+        Base.Docs.doc(f::$T) = Base.Docs.doc(PyObject(f))
+        # Code from https://github.com/JuliaPy/PyPlot.jl/blob/6b38c75/src/PyPlot.jl#L65-L71
+        Base.getproperty(f::$T, s::Symbol) = getproperty(PyObject(f), s)
+        Base.getproperty(f::$T, s::AbstractString) = getproperty(PyObject(f), s)
+        Base.setproperty!(f::$T, s::Symbol, x) = setproperty!(PyObject(f), s, x)
+        Base.setproperty!(f::$T, s::AbstractString, x) = setproperty!(PyObject(f), s, x)
+        PyCall.hasproperty(f::$T, s::Symbol) = hasproperty(PyObject(f), s)
+        Base.propertynames(f::$T) = propertynames(PyObject(f))
+        Base.haskey(f::$T, x) = haskey(PyObject(f), x)
+    end)
+end
+
+end # module Clients
