9de5eb0a0873ca452dbed4db440ffaf925d463bc
Qi Zhang <singularitti@outlook.com>
2020-01-24 16:24:51 -0500

    Update `__init__`

--- src/init.jl
+++ src/init.jl
@@ -8,7 +8,16 @@ export paramiko
 const paramiko = PyNULL()
 
 function __init__()
-    copy!(paramiko, pyimport("paramiko"))
+    try
+        copy!(paramiko, pyimport("paramiko"))
+    catch e
+        e isa PyCall.PyError && println("`paramiko` is not found! I will try to install it!")
+        try
+            @eval using Pkg
+            haskey(Pkg.installed(), "Conda") || @eval Pkg.add("Conda")
+        catch
+        end
+    end
     # Code from https://github.com/JuliaPy/PyPlot.jl/blob/caf7f89/src/init.jl#L168-L173
     vers = paramiko.__version__
     global version = try
