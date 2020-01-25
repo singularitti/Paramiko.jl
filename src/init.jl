efff91aed450064d7c414bde1827a6debafc7226
Qi Zhang <singularitti@outlook.com>
2020-01-24 22:27:21 -0500

    Add method `pyinterface`

--- src/init.jl
+++ src/init.jl
@@ -1,7 +1,9 @@
 __precompile__() # this module is safe to precompile
 
-using PyCall: PyNULL, PyError, pyimport
+using PyCall: PyNULL, PyError, PyObject, PyAny, pyimport, pycall
 using VersionParsing: vparse
+# Extending methods
+import PyCall
 
 export paramiko
 
@@ -35,3 +37,28 @@ function __init__()
         v"0.0.0" # fallback
     end
 end
+
+function pyinterface(T::Symbol)
+    return quote
+        # Code from https://github.com/JuliaPy/PyPlot.jl/blob/6b38c75/src/PyPlot.jl#L54-L62
+        PyCall.PyObject(f::$T) = getfield(f, :o)
+        Base.convert(::Type{$T}, o::PyObject) = $T(o)
+        Base.:(==)(f::$T, g::$T) = PyObject(f) == PyObject(g)
+        Base.:(==)(f::$T, g::PyObject) = PyObject(f) == g
+        Base.:(==)(f::PyObject, g::$T) = f == PyObject(g)
+        Base.hash(f::$T) = hash(PyObject(f))
+        PyCall.pycall(f::$T, args...; kws...) = pycall(PyObject(f), args...; kws...)
+        (f::$T)(args...; kws...) = pycall(PyObject(f), PyAny, args...; kws...)
+        Base.Docs.doc(f::$T) = Base.Docs.doc(PyObject(f))
+        # Code from https://github.com/JuliaPy/PyPlot.jl/blob/6b38c75/src/PyPlot.jl#L65-L71
+        Base.getproperty(f::$T, s::Symbol) = getproperty(PyObject(f), s)
+        Base.getproperty(f::$T, s::AbstractString) = getproperty(PyObject(f), s)
+        Base.setproperty!(f::$T, s::Symbol, x) = setproperty!(PyObject(f), s, x)
+        Base.setproperty!(f::$T, s::AbstractString, x) = setproperty!(PyObject(f), s, x)
+        PyCall.hasproperty(f::$T, s::Symbol) = hasproperty(PyObject(f), s)
+        Base.propertynames(f::$T) = propertynames(PyObject(f))
+        Base.haskey(f::$T, x) = haskey(PyObject(f), x)
+        # Common methods
+        Base.close(f::$T) = PyObject(f).close()
+    end
+end # function pyinterface
