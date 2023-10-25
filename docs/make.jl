aa9684d8c50f7e962d31fc77ea60d16e0c005954
singularitti <singularitti@outlook.com>
2023-10-25 19:51:08 -0400

    Update docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,17 @@
 using Paramiko
 using Documenter
 
+DocMeta.setdocmeta!(Paramiko, :DocTestSetup, :(using Paramiko); recursive=true)
+
 makedocs(;
     modules=[Paramiko],
-    authors="Qi Zhang <singularitti@outlook.com>",
-    repo="https://github.com/singularitti/Paramiko.jl/blob/{commit}{path}#L{line}",
+    authors="singularitti <singularitti@outlook.com> and contributors",
+    repo="https://github.com/singularitti/Paramiko.jl/blob/{commit}{path}#{line}",
     sitename="Paramiko.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://singularitti.github.io/Paramiko.jl",
+        edit_link="main",
         assets=String[],
     ),
     pages=[
@@ -18,4 +21,5 @@ makedocs(;
 
 deploydocs(;
     repo="github.com/singularitti/Paramiko.jl",
+    devbranch="main",
 )
